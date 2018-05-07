03f60850576252c7b8c8cf81eeaf42510bfb3008
Anthony Degleris <degleris1@gmail.com>
2018-05-07 16:50:14 -0700

    added CNMF class docstrings

--- cnmfpy/cnmf.py
+++ cnmfpy/cnmf.py
@@ -19,9 +19,36 @@ class CNMF(object):
     def __init__(self, n_components, maxlag, tol=1e-5, n_iter_max=100,
                  l2_scfo=1e-6, l1_W=0.0, l1_H=0.0):
         """
-        l1_W (float) : strength of sparsity penalty on W
-        l1_H (float) : strength of sparsity penalty on H
-        maxlag (int) : (L-1)/2
+        Convolutive Non-Negative Matrix Factorization (CNMF)
+        
+        Factors a matrix into a convolution between a tensor `W` and a
+        matrix `H`.
+
+        Parameters
+        ----------
+        n_components : int
+            Number of components to fit.
+        maxlag : int
+            Maximum time lag in each sequence. A single sequence can lag up to
+            `maxlag` entries left or right and has length `2*maxlag+1`.
+        tol : float, optional
+            Tolerance for convergence. If the change in cost is less than the
+            `tol`, the algorithm will terminate early.
+        n_iter_max : int, optional
+            Maximum number of iterations during algorithm fitting.
+        l2_scfo : float, optional
+            Weight of the soft cross-factors orthogonality regularizer. See
+            references for details.
+        l1_W : float, optional
+            Weight of the L1 regularizer for the entries of `W`.
+        l1_H : float, optional
+            Weight of the L1 regularizer for the entries of `H`.
+
+        References
+        ----------
+        See Mackevicius, Bahle, et al., *Unsupervised discovery of temporal 
+        sequences in high-dimensional datasets, with applications to 
+        neuroscience.*
         """
         self.n_components = n_components
         self.maxlag = maxlag
@@ -42,6 +69,21 @@ class CNMF(object):
 
 
     def fit(self, data, alg='mult'):
+        """
+        Fit a CNMF model to the data.
+
+        Parameters
+        ----------
+        data : array-like, shape (n_time, n_features)
+            Training data to fit.
+        alg : string {'mult', 'bcd'}, optional
+            Algorithm used to fit the data.
+
+        Returns
+        -------
+        self : object
+            Returns the instance itself.
+        """
         # Check input
         if (data < 0).any():
             raise ValueError('Negative values in data to fit')
@@ -70,10 +112,17 @@ class CNMF(object):
         self.W = self.W[:, :, ind]
         self.H.assign(self.H.shift(0)[ind, :])
 
+        return self
+
 
     def predict(self):
         """
         Return low-rank reconstruction of data.
+
+        Returns
+        -------
+        est : array-like, shape (n_time, n_features)
+            Reconstruction of the data using `W` and `H`.
         """
         # check that W and H are fit
         self._check_is_fitted()
@@ -82,6 +131,9 @@ class CNMF(object):
        
 
     def _check_is_fitted(self):
+        """
+        Check if `W`, `H` have been fitted.
+        """
         if self.W is None or self.H is None:
             raise ValueError('This ConvNMF instance is not fitted yet.'
                              'Call \'fit\' with appropriate arguments '
