8514ea5eac3ee38ffbfd20c50359bb6eb16252ae
Anthony Degleris <degleris1@gmail.com>
2018-05-20 22:40:24 -0700

    documented conv.py

--- cnmfpy/conv.py
+++ cnmfpy/conv.py
@@ -2,18 +2,21 @@ import numpy as np
 
 # TODO: subclass np.ndarray?
 class ShiftMatrix(object):
-    """
-    Thin wrapper around a numpy matrix to support shifting along the second
-    axis and padding with zeros.
-    """
-
     def __init__(self, X, L):
         """
-        X : numpy 2-d array
-        L : int, largest shift
-        """
+        Shift Matrix.
+        
+        Thin wrapper around a numpy ndarray to support shifting along the second
+        axis and padding with zeros.
 
-        # ShiftMatrix behaves like the original matrix
+        Parameters
+        ----------
+        X : numpy, shape (n1, n2)
+            Numpy array to be wrapped.
+        L : int
+            Largest shift in either direction.
+        """
+        # behaves like the original ndarray
         self.shape = X.shape
         self.size = X.size
 
@@ -21,15 +24,37 @@ class ShiftMatrix(object):
         self.L = L
         self.X = np.pad(X, ((0, 0), (L, L)), mode='constant')
 
+
     def shift(self, l):
-        """Shifts the columns left, padding with zeros on the right."""
+        """
+        Shifts the columns left, padding with zeros on the right.
+
+        Parameters
+        ----------
+        l : int
+            Number of times to shift left.
+
+        Returns
+        -------
+        X_shifted : ndarray
+            Returns a shifted version of the original ndarray.
+        """
         if np.abs(l) > self.L:
             raise ValueError('requested too large of a shift.' + str(np.abs(l)) + ' > ' + str(self.L))
 
         r = slice(self.L - l, self.L + self.shape[1] - l)
         return self.X[:, r]
 
+
     def assign(self, Xnew):
+        """
+        Reassigns the numpy ndarray.
+
+        Parameters
+        ----------
+        Xnew : ndarray, shape (n1, n2)
+            A new ndarray of the same shape as before.
+        """
         self.X[:, self.L:-self.L] = Xnew
 
 
@@ -37,8 +62,19 @@ class ShiftMatrix(object):
 def tensor_conv(W, H):
     """
     Convolves a tensor W and ShiftMatrix H.
-    """
-   
+
+    Parameters
+    ----------
+    W : ndarray, shape (n_lag, n_neurons, n_components)
+        Tensor of neural sequences.
+    H : ShiftMatrix, shape (n_components, n_time)
+        ShiftMatrix of time componenents.
+
+    Returns
+    -------
+    X : ndarray, shape (n_neurons, n_time)
+        Tensor convolution of W and H.
+    """   
     # preallocate result
     m, n = W.shape[1], H.shape[1]
     result = np.zeros((m, n))
@@ -55,8 +91,20 @@ def tensor_conv(W, H):
 def tensor_transconv(W, X):
     """
     Transpose tensor convolution of tensor W and ShiftMatrix X.
-    """
 
+    Parameters
+    ----------
+    W : ndarray, shape (n_lag, n_neurons, n_components)
+        Tensor of neural sequences.
+    H : ShiftMatrix, shape (n_components, n_time)
+        ShiftMatrix of time componenents.
+
+    Returns
+    -------
+    X : ndarray, shape (n_neurons, n_time)
+        Transpose tensor convolution of W and H, i.e. the tensor convolution
+        but shifted the opposite direction.
+    """
     # preallocate result
     m, n = W.shape[2], X.shape[1]
     result = np.zeros((m, n))
