refactor algorithm interface, fix gradient tests

cmfpy/algs/gradient_descent.py
@@ -1,26 +1,27 @@
+import numba
+
 import numpy as np
 import numpy.linalg as la
 from numbers import Integral
 
-from ..common import grad_W, grad_H, proj_step
+from ..common import cmf_predict, s_dot, s_T_dot
+from .base import AbstractOptimizer
 
 
-class GradDescent:
+class GradDescent(AbstractOptimizer):
     """
     Gradient Descent update rules for CMF.
     """
 
-    def __init__(self, model, data, tol=1e-5, patience=3, step_decrement=5.):
-        """Initialize Algorithm."""
+    def __init__(self, data, initW, initH, tol=1e-5, patience=3,
+                 step_decrement=5.):
 
         # Check inputs,
         if patience < 1 or not isinstance(patience, Integral):
             raise ValueError("Patience must be a positive integer.")
 
-        # Save data and model parameters.
-        self.X = data
-        self.W = model.W
-        self.H = model.H
+        # Invoke super class initialization procedures.
+        super().__init__(data, initW, initH)
 
         # TODO: Figure out whether this is a reasonable guess.
         self.step_size = 1 / la.norm(data)
@@ -34,12 +35,43 @@ class GradDescent:
         self.gW = np.empty_like(self.W)
         self.gH = np.empty_like(self.H)
 
+        # Cache useful intermediate results. Norm of data matrix,
+        # residuals, and gradients.
+        self.normX = la.norm(data)
+        self.cache_resids()
+        self.cache_gW()
+        self.cache_gH()
+
+    def cache_resids(self):
+        """Updates intermediate computations."""
+        self.est = cmf_predict(self.W, self.H)
+        self.resids = self.est - self.X
+
+    def cache_gW(self):
+        """Gradient of W. Stores result in `gW`."""
+        for lag in range(self.W.shape[0]):
+            self.gW[lag] = s_T_dot(self.resids, self.H, lag)
+        return self.gW
+
+    def cache_gH(self):
+        """Gradient of H. Stores result in `gH`."""
+        self.gH.fill(0.)
+        for lag, Wl in enumerate(self.W):
+            self.gH += s_dot(Wl.T, self.resids, -lag)
+        return self.gH
+
     def update(self):
         """Update parameters."""
-        grad_W(self.X, model.H, self.gW)
-        grad_H(self.X, model.W, self.gH)
-        self.W = proj_step(model.W, self.gW, self.step_size)
-        self.H = proj_step(model.W, self.gH, self.step_size)
+
+        # Update parameters.
+        _projected_step(self.W, -self.step_size * self.gW)
+        _projected_step(self.H, -self.step_size * self.gH)
+
+        # Update residuals and gradients and return loss
+        self.cache_resids()
+        self.cache_gW()
+        self.cache_gH()
+        return self.loss()
 
     def converged(self, loss_hist):
         """Check if converged, decrease step size if needed."""
@@ -60,6 +92,16 @@ class GradDescent:
         else:
             return False
 
+    @property
+    def unnormalized_loss(self):
+        """Compute unnormalized loss (for testing gradients)."""
+        return 0.5 * la.norm(self.resids) ** 2
+
+    @property
+    def loss(self):
+        """Compute normalized loss (for user-facing functions)."""
+        return la.norm(self.resids) / self.normX
+
 
 class BlockDescent(GradDescent):
     """
@@ -68,7 +110,29 @@ class BlockDescent(GradDescent):
 
     def update(self):
         """Overrides gradient update rule. Same idea, but different order."""
-        grad_W(data, model.H, self.gW)
-        self.W = proj_step(model.W, self.gW, self.step_size)
-        grad_H(data, model.W, self.gH)
-        self.H = proj_step(model.W, self.gH, self.step_size)
+
+        # Update W (gradient should be up-to-date)
+        _projected_step(self.W, -self.step_size * self.gW)
+
+        # Update H (need to recompute residuals since W was updated).
+        self.cache_resids()
+        self.cache_gH()
+        _projected_step(self.H, -self.step_size * self.gH)
+
+        # Update residuals and gradient computation for W (for next iteration).
+        self.cache_resids()
+        self.cache_gW()
+
+        # Return loss
+        return self.loss
+
+
+# TODO: parallel loop here may be overkill
+@numba.jit(nopython=True, parallel=True)
+def _projected_step(X, dX):
+    """
+    Adds dX to X and projects onto nonegative orthant. Modifies X in-place.
+    """
+    x, dx = X.ravel(), dX.ravel()
+    for i in numba.prange(len(x)):
+        x[i] = np.maximum(x[i] + dx[i], 0.0)

cmfpy/model.py
@@ -12,6 +12,11 @@ from . import initialize
 
 from .common import cmf_predict
 
+NOT_FITTED_ERROR = ValueError(
+    "This CMF instance is not fitted yet. Call 'fit' with appropriate"
+    "arguments before using this method."
+)
+
 
 class CMF(object):
     """
@@ -21,9 +26,9 @@ class CMF(object):
     matrix `H`.
     """
 
-    def __init__(self, n_components, maxlag, tol=1e-5, n_iter_max=100,
-                 l2_scfo=0, l1_W=0.0, l1_H=0.0, verbose=True,
-                 method='mult'):
+    def __init__(self, n_components, maxlag, n_iter_max=100,
+                 l1_W=0.0, l1_H=0.0, verbose=True, alg_name='mult',
+                 **alg_opts):
         """
         Parameters
         ----------
@@ -44,28 +49,23 @@ class CMF(object):
             Weight of the L1 regularizer for the entries of `W`.
         l1_H : float, optional
             Weight of the L1 regularizer for the entries of `H`.
-
-        References
-        ----------
-        See Mackevicius, Bahle, et al., *Unsupervised discovery of temporal
-        sequences in high-dimensional datasets, with applications to
-        neuroscience.*
+        alg_name : str
+            String specifying algorithm.
+        **alg_opts : dict
+            Additional keyword arguments are passed as algorithm-specific
+            options when, such as learning rate and convergence tolerance.
+            See algorithm classes for details.
         """
         self.n_components = n_components
         self.maxlag = maxlag
 
-        self.W = None
-        self.H = None
-
-        self.tol = tol
         self.n_iter_max = n_iter_max
 
-        self.l2_scfo = l2_scfo
         self.l1_W = l1_W
         self.l1_H = l1_H
 
-        self.loss_hist = None
-        self.method = method
+        self.alg_name = alg_name
+        self.alg_opts = alg_opts
         self.verbose = verbose
 
     def fit(self, data):
@@ -90,64 +90,68 @@ class CMF(object):
         # Initialize W and H.
         n_features, n_time = data.shape
 
-        self.W = np.abs(npr.rand(self.maxlag, n_features, self.n_components))
-        self.H = np.abs(npr.rand(self.n_components, n_time))
+        W = npr.rand(self.maxlag, n_features, self.n_components)
+        H = npr.rand(self.n_components, n_time)
 
         alpha = (data * self.predict()).sum() / la.norm(est)**2
 
-        self.W = alpha * W
-        self.H = alpha * H
+        # Create algorithm helper class.
+        alg_class = ALGORITHMS[self.alg_name]
+        algorithm = alg_class(data, W, H, **self.alg_opts)
 
         # Set up optimization tracking.
-        model.loss_hist = [model.score(data)]
-        model.time_hist = [0.0]
-        iterations = trange(self.n_iter_max) if verbose else range(self.n_iter_max)
+        self.loss_hist = [self.score(data)]
+        self.time_hist = [0.0]
+        if verbose:
+            iterations = trange(self.n_iter_max)
+        else:
+            iterations = range(self.n_iter_max)
 
-        # Initialize algorithm class.
-        algorithm = ALGORITHMS[self.method](self, data)
-
+        # Run optimization.
         for itr in iterations:
 
             # Time each parameter update.
             t0 = time.time()
 
             # Update model parameters.
-            self.W, self.H = alg.update()
+            loss = alg.update()
 
             # Record time of parameter update.
             dur = time.time() - t0
             self.time_hist.append(self.time_hist[-1] + dur)
-            self.loss_hist.append(self.score(data))
-
-            # Renormalize factors to prevent singularities.
-            model.W, model.H = renormalize(model.W, model.H)
+            self.loss_hist.append(loss)
 
             # Check convergence.
-            if alg.converged(model.loss_hist):
+            if algorithm.converged():
                 break
 
+        # Extract model parameters from algorithm class.
+        self._W = alg.W
+        self._H = alg.H
+
+    def sort_components(self):
+        """
+        Sorts model components by explanatory power.
+        """
+
         # Compute explanatory power of each factor.
-        loadings = compute_loadings(data, self.W, self.H)
+        loadings = compute_loadings(data, self.motifs, self.factors)
 
-        # Sort factors by power
+        # Sort components by power
         ind = np.argsort(loadings)
-        self.W = self.W[:, :, ind]
-        self.H = self.H[ind, :]
-
-        return self
+        self._W = self._W[:, :, ind]  # motifs
+        self._H = self._H[ind, :]     # factors
 
     def predict(self):
         """
-        Return low-rank reconstruction of data.
+        Computes low-rank reconstruction of data.
 
         Returns
         -------
         est : array-like, shape (n_time, n_features)
-            Reconstruction of the data using `W` and `H`.
+            Model estimate of the data.
         """
-        # check that W and H are fit
-        self._check_is_fitted()
-        return cmf_predict(self.W, self.H)
+        return cmf_predict(self.motifs, self.factors)
 
     def score(self, data):
         """
@@ -168,17 +172,23 @@ class CMF(object):
         TODO: demean the data
         """
         error = self.predict() - data
-
         return 1 - (la.norm(error)**2 / la.norm(data)**2)
 
-    def _check_is_fitted(self):
-        """
-        Check if `W`, `H` have been fitted.
-        """
-        if self.W is None or self.H is None:
-            raise ValueError('This ConvNMF instance is not fitted yet.'
-                             'Call \'fit\' with appropriate arguments '
-                             'before using this method.')
+    @property
+    def motifs(self):
+        """Returns `W` (lags x features x components) parameters."""
+        try:
+            return self._W
+        except AttributeError:
+            raise NOT_FITTED_ERROR
+
+    @property
+    def factors(self):
+        """Returns `H` (components x timebins) parameters."""
+        try:
+            return self._H
+        except AttributeError:
+            raise NOT_FITTED_ERROR
 
 
 def compute_loadings(data, W, H):

tests/test_grads.py
@@ -6,53 +6,55 @@ from numpy.testing import assert_allclose
 
 from cmfpy import CMF
 from cmfpy.initialize import init_rand
-from cmfpy.common import grad_W, grad_H, cmf_loss
+from cmfpy.algs.gradient_descent import GradDescent, BlockDescent
 from cmfpy.datasets import Synthetic
 
 from scipy.optimize import approx_fprime
 
 # Test parameters and tolerances.
-TOL = 1e-5
-EPS = 100 * np.sqrt(np.finfo(float).eps)
+TOL = 1e-4
+EPS = 1e-7
 SEED = 1234
+N, T = 10, 200
 DATA = Synthetic(
-    n_features=10,
-    n_timebins=200,
+    n_features=N,
+    n_timebins=T,
     sparsity=.2,
 ).generate()
 
 
+@pytest.mark.parametrize("algclass", [GradDescent, BlockDescent])
 @pytest.mark.parametrize("L", [5, 10])
 @pytest.mark.parametrize("K", [1, 5])
-def test_gradients(L, K):
+def test_gradients(algclass, L, K):
 
-    # Initialize model parameters.
+    # Initialize parameters.
     rs = np.random.RandomState(SEED)
-    model = CMF(n_components=K, maxlag=L)
-    W, H = init_rand(model, DATA, random_state=rs)
+    W = rs.rand(L, N, K)
+    H = rs.rand(K, T)
 
-    # Thin wrapper around loss functions for gradient checking.
+    # Initialize algorithm.
+    alg = algclass(DATA, W.copy(), H.copy())
+
+    # Computed gradients.
+    gW = alg.gW.copy()
+    gH = alg.gH.copy()
+
+    # Do a gradient check by finite differencing.
     def loss_W(w_vec):
-        w = w_vec.reshape(W.shape)
-        return cmf_loss(DATA, w, H)
+        _W = w_vec.reshape((L, N, K))
+        _alg = algclass(DATA, _W, H.copy())
+        return _alg.unnormalized_loss
 
     def loss_H(h_vec):
-        h = h_vec.reshape(H.shape)
-        return cmf_loss(DATA, W, h)
+        _H = h_vec.reshape((K, T))
+        _alg = algclass(DATA, W.copy(), _H)
+        return _alg.unnormalized_loss
 
-    # # Compute approximate gradients.
-    # approx_gW = approx_fprime(W.ravel(), loss_W, EPS)
-    # approx_gH = approx_fprime(H.ravel(), loss_H, EPS)
-
-    # Compute gradients using internal code.
-    gW = np.zeros_like(W)
-    grad_W(DATA, H, gW)
-
-    gH = np.zeros_like(H)
-    grad_H(DATA, W, gH)
-
-    assert True
+    # Compute approximate gradients.
+    approx_gW = approx_fprime(W.ravel(), loss_W, EPS)
+    approx_gH = approx_fprime(H.ravel(), loss_H, EPS)
 
     # Check for agreement.
-    # assert_allclose(approx_gW, gW.ravel(), rtol=TOL)
-    # assert_allclose(approx_gH, gH.ravel(), rtol=TOL)
+    assert_allclose(approx_gW, gW.ravel(), rtol=TOL)
+    assert_allclose(approx_gH, gH.ravel(), rtol=TOL)